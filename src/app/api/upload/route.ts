140ed32b0c628f27a8f6a80d1f644f567290111c
Suraj Chakraborty <76647754+suraj-chakraborty@users.noreply.github.com>
2025-06-18 21:35:19 +0530

    uploadDir fix

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -51,7 +51,7 @@ export async function POST(request: Request) {
       );
     }
 
-    const uploadDir = path.join(process.cwd(), '/tmp', 'uploads');
+    const uploadDir = path.join('/tmp', 'uploads');
     await mkdir(uploadDir, { recursive: true });
 
     const arrayBuffer = await file.arrayBuffer();
