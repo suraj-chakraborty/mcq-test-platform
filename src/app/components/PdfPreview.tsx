a0832acdeb8d8271f045692fcba8aecec70381a0
Suraj Chakraborty <surajchakraborty211@gmail.com>
2026-03-20 16:25:49 +0530

    loading fix

--- src/app/components/PdfPreview.tsx
+++ src/app/components/PdfPreview.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import Loading from '../loading';
+import { LoadingSpinner as Loading } from './LoadingSpinner';
 
 // Set up PDF.js worker
 if (typeof window !== 'undefined' && pdfjs.GlobalWorkerOptions) {
