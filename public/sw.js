636b091224b03a04984d32469c1098eccdacbb1b
suraj-chakraborty <surajchakraborty211@gmail.com>
2026-08-14 23:21:44 +0530

    add lightweight service worker to handle pwa routes

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Lightweight Service Worker to prevent 404 console noise and manage client caching
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
